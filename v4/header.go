e035802cfb084a4e6de881dc12d42d2edd0c5da9
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2026-01-05 06:20:36 +0300

    add ipv4 header

--- v4/header.go
+++ v4/header.go
@@ -12,6 +12,10 @@ import (
 	"github.com/name212/ip/utils"
 )
 
+const (
+	minHeaderLength = 20
+)
+
 type Protocol uint8
 
 // 1	Internet Control Message Protocol	ICMP
@@ -56,81 +60,19 @@ type Header struct {
 	SourceIP       net.IP
 	DestinationIP  net.IP
 	Options        []byte
+
+	flags Flags
 }
 
 type Flags struct {
+	IsEvil        bool
 	DontFragment  bool
 	MoreFragments bool
 }
 
-func (h *Header) IsValidVersion() bool {
-	return h.Version == 4
-}
-
-func (h *Header) GetProtocol() Protocol {
-	return Protocol(h.Protocol)
-}
-
-func (h *Header) GetFlags() Flags {
-	df := utils.CheckBit(h.Flags, 1)
-	mf := utils.CheckBit(h.Flags, 2)
-
-	return Flags{
-		DontFragment:  df,
-		MoreFragments: mf,
-	}
-}
-
-func (h *Header) ProtocolString() string {
-	str, ok := protocolsMap[Protocol(h.Protocol)]
-	if ok {
-		return str
-	}
-
-	return "Unknown"
-}
-
-func (h *Header) writeOptions(s *strings.Builder) {
-	if h.Options == nil {
-		s.WriteString(utils.FmtLn("No options set"))
-		return
-	}
-
-	opts, err := ParseOptions(h.Options)
-	if err != nil {
-		s.WriteString(utils.FmtLn("Cannot parse options: %v", err))
-		return
-	}
-
-	for _, opt := range opts {
-		msg := utils.FmtLnWithTabPrefix("%s: data len %d", opt.TypeLong(), len(opt.Data))
-		s.WriteString(msg)
-	}
-}
-
-func (h *Header) String() string {
-	s := strings.Builder{}
-
-	flags := h.GetFlags()
-
-	s.WriteString(utils.FmtLn("IsValidVersion: %v", h.IsValidVersion()))
-	s.WriteString(utils.FmtLn("Protocol: %s", h.ProtocolString()))
-	s.WriteString(utils.FmtLn("Source: %s", h.SourceIP.String()))
-	s.WriteString(utils.FmtLn("Destination: %s", h.DestinationIP.String()))
-	s.WriteString(utils.FmtLn("TTL: %d", h.TTL))
-	s.WriteString(utils.FmtLn("Size: %d", h.TotalLength))
-	s.WriteString(utils.FmtLn("Flags:"))
-	s.WriteString(utils.FmtLnWithTabPrefix("Don't Fragment: %v", flags.DontFragment))
-	s.WriteString(utils.FmtLnWithTabPrefix("More Fragments: %v", flags.MoreFragments))
-	h.writeOptions(&s)
-	s.WriteString(utils.FmtLn("Checksum: %d", h.Checksum))
-
-	return s.String()
-}
-
 // ParseIPv4Header parses the IPv4 header from the given byte slice
 func ParseIPv4Header(data []byte) (*Header, error) {
-	if len(data) < 20 {
+	if len(data) < minHeaderLength {
 		return nil, fmt.Errorf("data too short to contain an IPv4 header")
 	}
 
@@ -149,12 +91,34 @@ func ParseIPv4Header(data []byte) (*Header, error) {
 		DestinationIP:  net.IP(data[16:20]),
 	}
 
-	// Calculate the header length in bytes
-	headerLength := int(header.IHL) * 4
+	if !header.IsValidVersion() {
+		return nil, fmt.Errorf("invalid version %d", header.Version)
+	}
 
-	// Check if options exist
-	if headerLength > 20 && len(data) >= headerLength {
-		header.Options = data[20:headerLength]
+	headerLengthBytes := header.HeaderLen()
+
+	if header.TotalLength < minHeaderLength {
+		return nil, fmt.Errorf("invalid (too small) IP total length (%d < %d)", header.TotalLength, minHeaderLength)
+	}
+
+	if headerLengthBytes < minHeaderLength {
+		return nil, fmt.Errorf("invalid (too small) IP header length (%d < %d)", headerLengthBytes, minHeaderLength)
+	}
+
+	if headerLengthBytes > header.GetTotalLen() {
+		return nil, fmt.Errorf("invalid IP header length > IP length (%d > %d)", headerLengthBytes, header.TotalLength)
+	}
+
+	flags := parseFlags(header.Flags)
+
+	if flags.IsEvil {
+		return nil, fmt.Errorf("invalid IP header flags first bit set to 1")
+	}
+
+	header.flags = flags
+
+	if headerLengthBytes > 20 && len(data) >= headerLengthBytes {
+		header.Options = data[20:headerLengthBytes]
 	} else {
 		header.Options = nil
 	}
@@ -162,272 +126,120 @@ func ParseIPv4Header(data []byte) (*Header, error) {
 	return header, nil
 }
 
-type OptionType uint8
-
-// 0/0x00	EOOL	End of Option List
-// 1/0x01	NOP	No Operation
-// 2/0x02	SEC	Security (defunct)
-// 7/0x07	RR	Record Route
-// 10/0x0A	ZSU	Experimental Measurement
-// 11/0x0B	MTUP	MTU Probe
-// 12/0x0C	MTUR	MTU Reply
-// 15/0x0F	ENCODE	ENCODE
-// 25/0x19	QS	Quick-Start
-// 30/0x1E	EXP	RFC3692-style Experiment
-// 68/0x44	TS	Time Stamp
-// 82/0x52	TR	Traceroute
-// 94/0x5E	EXP	RFC3692-style Experiment
-// 130/0x82	SEC	Security (RIPSO)
-// 131/0x83	LSR	Loose Source Route
-// 133/0x85	E-SEC	Extended Security (RIPSO)
-// 134/0x86	CIPSO	Commercial IP Security Option
-// 136/0x88	SID	Stream ID
-// 137/0x89	SSR	Strict Source Route
-// 142/0x8E	VISA	Experimental Access Control
-// 144/0x90	IMITD	IMI Traffic Descriptor
-// 145/0x91	EIP	Extended Internet Protocol
-// 147/0x93	ADDEXT	Address Extension
-// 148/0x94	RTRALT	Router Alert
-// 149/0x95	SDB	Selective Directed Broadcast
-// 151/0x97	DPS	Dynamic Packet State
-// 152/0x98	UMP	Upstream Multicast Packet
-// 158/0x9E	EXP	RFC3692-style Experiment
-// 205/0xCD	FINN	Experimental Flow Control
-// 222/0xDE	EXP	RFC3692-style Experiment
-const (
-	OptionEndOfList                    OptionType = 0
-	OptionNoOperation                  OptionType = 1
-	OptionSecurityDefunct              OptionType = 2
-	OptionRecordRoute                  OptionType = 7
-	OptionExperimentalMeasurement      OptionType = 10
-	OptionMTUProbe                     OptionType = 11
-	OptionMTUReply                     OptionType = 12
-	OptionENCODE                       OptionType = 15
-	OptionQuickStart                   OptionType = 25
-	OptionRFC3692StyleExperimentFirst  OptionType = 30
-	OptionTimeStamp                    OptionType = 68
-	OptionTraceroute                   OptionType = 82
-	OptionRFC3692StyleExperimentSecond OptionType = 94
-	OptionSecurityRIPSO                OptionType = 130
-	OptionLooseSourceRoute             OptionType = 131
-	OptionExtendedSecurityRIPSO        OptionType = 133
-	OptionCommercialIPSecurityOption   OptionType = 134
-	OptionStreamID                     OptionType = 136
-	OptionStrictSourceRoute            OptionType = 137
-	OptionExperimentalAccessControl    OptionType = 142
-	OptionIMITrafficDescriptor         OptionType = 144
-	OptionExtendedInternetProtocol     OptionType = 145
-	OptionAddressExtension             OptionType = 147
-	OptionRouterAlert                  OptionType = 148
-	OptionSelectiveDirectedBroadcast   OptionType = 149
-	OptionDynamicPacketState           OptionType = 151
-	OptionUpstreamMulticastPacket      OptionType = 152
-	OptionRFC3692StyleExperimentThird  OptionType = 158
-	OptionExperimentalFlowControl      OptionType = 205
-	OptionRFC3692StyleExperimentFour   OptionType = 222
-)
-
-type optionDescription struct {
-	short string
-	long  string
+func (h *Header) IsValidVersion() bool {
+	return h.Version == 4
 }
 
-var optionTypesMap = map[OptionType]*optionDescription{
-	OptionEndOfList: &optionDescription{
-		short: "EOOL",
-		long:  "End Of List",
-	},
-	OptionNoOperation: &optionDescription{
-		short: "NOP",
-		long:  "No Operation",
-	},
-	OptionSecurityDefunct: &optionDescription{
-		short: "SEC",
-		long:  "Security Defunct",
-	},
-	OptionRecordRoute: &optionDescription{
-		short: "ROR",
-		long:  "Record Route",
-	},
-	OptionExperimentalMeasurement: &optionDescription{
-		short: "EXP",
-		long:  "Experimental Measurement",
-	},
-	OptionMTUProbe: &optionDescription{
-		short: "MTUP",
-		long:  "MTU Probe",
-	},
-	OptionMTUReply: &optionDescription{
-		short: "MTUR",
-		long:  "MTU Reply",
-	},
-	OptionENCODE: &optionDescription{
-		short: "ENCODE",
-		long:  "ENCODE",
-	},
-	OptionQuickStart: &optionDescription{
-		short: "QS",
-		long:  "Quick Start",
-	},
-	OptionRFC3692StyleExperimentFirst: &optionDescription{
-		short: "EXP",
-		long:  "RFC3692 Experiment First",
-	},
-	OptionTimeStamp: &optionDescription{
-		short: "TS",
-		long:  "Timestamp",
-	},
-	OptionTraceroute: &optionDescription{
-		short: "TR",
-		long:  "Traceroute",
-	},
-	OptionRFC3692StyleExperimentSecond: &optionDescription{
-		short: "EXP",
-		long:  "RFC3692 Experiment Second",
-	},
-	OptionSecurityRIPSO: &optionDescription{
-		short: "SEC",
-		long:  "Security RIPSO",
-	},
-	OptionLooseSourceRoute: &optionDescription{
-		short: "LSR",
-		long:  "Loose Source Route",
-	},
-	OptionExtendedSecurityRIPSO: &optionDescription{
-		short: "E-SEC",
-		long:  "Extended Security (RIPSO)",
-	},
-	OptionCommercialIPSecurityOption: &optionDescription{
-		short: "CIPSO",
-		long:  "Commercial IP Security Option",
-	},
-	OptionStreamID: &optionDescription{
-		short: "SID",
-		long:  "Stream ID",
-	},
-	OptionStrictSourceRoute: &optionDescription{
-		short: "SSR",
-		long:  "Strict Source Route",
-	},
-	OptionExperimentalAccessControl: &optionDescription{
-		short: "VISA",
-		long:  "Experimental Access Control",
-	},
-	OptionIMITrafficDescriptor: &optionDescription{
-		short: "IMITD",
-		long:  "IMI Traffic Descriptor",
-	},
-	OptionExtendedInternetProtocol: &optionDescription{
-		short: "EIP",
-		long:  "Extended Internet Protocol",
-	},
-	OptionAddressExtension: &optionDescription{
-		short: "ADDEXT",
-		long:  "Address Extension",
-	},
-	OptionRouterAlert: &optionDescription{
-		short: "RTRALT",
-		long:  "Router Alert",
-	},
-	OptionSelectiveDirectedBroadcast: &optionDescription{
-		short: "SDB",
-		long:  "Selective Directed Broadcast",
-	},
-	OptionDynamicPacketState: &optionDescription{
-		short: "DPS",
-		long:  "Dynamic Packet State",
-	},
-	OptionUpstreamMulticastPacket: &optionDescription{
-		short: "UMP",
-		long:  "Upstream Multicast Packet",
-	},
-	OptionRFC3692StyleExperimentThird: &optionDescription{
-		short: "EXP",
-		long:  "RFC3692 Experiment Third",
-	},
-	OptionExperimentalFlowControl: &optionDescription{
-		short: "EXPF",
-		long:  "Experimental Flow Control",
-	},
-	OptionRFC3692StyleExperimentFour: &optionDescription{
-		short: "EXP",
-		long:  "RFC3692 Experiment Four",
-	},
+func (h *Header) GetTotalLen() int {
+	return int(h.TotalLength)
 }
 
-func unknownOptionDescription(optionType OptionType) *optionDescription {
-	return &optionDescription{
-		short: "UNKNOWN",
-		long:  fmt.Sprintf("Unknown: %d", optionType),
-	}
+func (h *Header) GetProtocol() Protocol {
+	return Protocol(h.Protocol)
 }
 
-type Option struct {
-	Type   uint8
-	Length uint8
-	Data   []byte
+func (h *Header) GetFlags() Flags {
+	return h.flags
 }
 
-func (o *Option) TypeShort() string {
-	desc, ok := optionTypesMap[OptionType(o.Type)]
+func (h *Header) ProtocolString() string {
+	str, ok := protocolsMap[Protocol(h.Protocol)]
 	if ok {
-		return desc.short
+		return str
 	}
 
-	return unknownOptionDescription(OptionType(o.Type)).short
+	return "Unknown"
 }
 
-func (o *Option) TypeLong() string {
-	desc, ok := optionTypesMap[OptionType(o.Type)]
-	if ok {
-		return desc.long
+func (h *Header) HeaderLen() int {
+	return int(h.IHL) * 4
+}
+
+func (h *Header) String() string {
+	s := strings.Builder{}
+
+	flags := h.GetFlags()
+
+	s.WriteString(utils.FmtLn("Source: %s", h.SourceIP.String()))
+	s.WriteString(utils.FmtLn("Destination: %s", h.DestinationIP.String()))
+	s.WriteString(utils.FmtLn("Protocol: %s", h.ProtocolString()))
+	s.WriteString(utils.FmtLn("TTL: %d", h.TTL))
+	s.WriteString(utils.FmtLn("Size: %d", h.TotalLength))
+	s.WriteString(utils.FmtLn("Flags:"))
+	s.WriteString(utils.FmtLnWithTabPrefix("Don't Fragment: %v", flags.DontFragment))
+	s.WriteString(utils.FmtLnWithTabPrefix("More Fragments: %v", flags.MoreFragments))
+	h.writeOptions(&s)
+	s.WriteString(utils.FmtLn("Checksum: %d", h.Checksum))
+
+	return s.String()
+}
+
+func (h *Header) ParseOptions() ([]Option, error) {
+	if h.Options == nil {
+		return nil, nil
 	}
 
-	return unknownOptionDescription(OptionType(o.Type)).long
-}
+	data := h.Options
+	res := make([]Option, 0, 4)
 
-func (o *Option) String() string {
-	return fmt.Sprintf("Option Type: %s(%d), Length: %d, Data: %v", o.TypeShort(), o.Type, o.Length, o.Data)
-}
+	for len(data) > 0 {
+		opt := Option{Type: data[0]}
 
-func ParseOptions(data []byte) ([]Option, error) {
-	var options []Option
-	offset := 0
-
-	for offset < len(data) {
-		if len(data)-offset < 1 {
-			return nil, fmt.Errorf("insufficient data for parsing options")
-		}
-
-		optionType := data[offset]
-		offset++
-
-		if optionType == 0 { // End of options list
-			break
-		} else if optionType == 1 { // No operation (NOP)
-			options = append(options, Option{Type: optionType})
-			continue
-		} else {
-			if len(data)-offset < 1 {
-				return nil, fmt.Errorf("insufficient data for option length")
+		switch opt.GetType() {
+		case OptionEndOfList: // End of options
+			return res, nil
+		case OptionNoOperation: // 1 byte padding
+			opt.Length = 1
+			data = data[1:]
+			res = append(res, opt)
+		default:
+			shortWithIDDesc := opt.TypeShortWithID()
+			if len(data) < 2 {
+				return nil, fmt.Errorf("invalid option length. Length %d less than 2 for %s", len(data), shortWithIDDesc)
 			}
-			optionLength := data[offset]
-			offset++
-
-			if optionLength < 2 || offset+int(optionLength)-2 > len(data) {
-				return nil, fmt.Errorf("invalid option length")
+			opt.Length = data[1]
+			if len(data) < int(opt.Length) {
+				return nil, fmt.Errorf("IP option length exceeds remaining IP header size, option for %s length %v", shortWithIDDesc, opt.Length)
 			}
-
-			optionData := data[offset : offset+int(optionLength)-2]
-			offset += int(optionLength) - 2
-
-			options = append(options, Option{
-				Type:   optionType,
-				Length: optionLength,
-				Data:   optionData,
-			})
+			if opt.Length <= 2 {
+				return nil, fmt.Errorf("invalid IP option type %s length %d. Must be greater than 2", shortWithIDDesc, opt.Length)
+			}
+			opt.Data = data[2:opt.Length]
+			data = data[opt.Length:]
+			res = append(res, opt)
 		}
 	}
 
-	return options, nil
+	return res, nil
+}
+
+func (h *Header) writeOptions(s *strings.Builder) {
+	if h.Options == nil {
+		s.WriteString(utils.FmtLn("No options set"))
+		return
+	}
+
+	opts, err := h.ParseOptions()
+	if err != nil {
+		s.WriteString(utils.FmtLn("Cannot parse options: %v", err))
+		return
+	}
+
+	s.WriteString(utils.FmtLn("Options:"))
+	for _, opt := range opts {
+		msg := utils.FmtLnWithTabPrefix("%s: data len %d", opt.TypeLong(), len(opt.Data))
+		s.WriteString(msg)
+	}
+}
+
+func parseFlags(f uint8) Flags {
+	evil := utils.CheckBit(f, 0)
+	df := utils.CheckBit(f, 1)
+	mf := utils.CheckBit(f, 2)
+
+	return Flags{
+		IsEvil:        evil,
+		DontFragment:  df,
+		MoreFragments: mf,
+	}
 }
--- v4/header_test.go
+++ v4/header_test.go
@@ -15,77 +15,137 @@ func TestParseHeader(t *testing.T) {
 		0x40, 0x06, 0xb1, 0xe6,
 		0xc0, 0xa8, 0x00, 0x68,
 		0xc0, 0xa8, 0x00, 0x01,
-		// Options and data would follow here if any
 	}
 
 	header, err := ParseIPv4Header(ipPacket)
 	if err != nil {
 		t.Error("Cannot parse header", err)
+		return
 	}
 
 	if !header.IsValidVersion() {
 		t.Error("Invalid version")
+		return
 	}
 
 	if header.GetProtocol() != ProtocolTCP {
 		t.Errorf("Invalid protocol: %d", header.Protocol)
+		return
 	}
 
 	if header.ProtocolString() != "TCP" {
 		t.Errorf("Invalid protocol string: %s", header.ProtocolString())
+		return
 	}
 
 	if !header.SourceIP.Equal(net.ParseIP("192.168.0.104")) {
 		t.Error("Invalid source ip")
+		return
 	}
+
 	if !header.DestinationIP.Equal(net.ParseIP("192.168.0.1")) {
 		t.Error("Invalid source ip")
+		return
 	}
 
 	if header.TotalLength != 60 {
 		t.Errorf("Invalid total length: %d", header.TotalLength)
+		return
 	}
 
 	if header.TTL != 64 {
 		t.Errorf("Invalid TTL: %d", header.TTL)
+		return
 	}
 
 	flags := header.GetFlags()
 
+	if flags.IsEvil {
+		t.Error("Evil set")
+		return
+	}
+
 	if flags.MoreFragments {
 		t.Error("Invalid More fragments")
+		return
 	}
 
 	if !flags.DontFragment {
 		t.Error("Invalid dont fragment")
+		return
 	}
 
 	if header.Checksum != 45542 {
 		t.Errorf("Invalid checksum: %d", header.Checksum)
+		return
 	}
 }
 
 func TestParseOptions(t *testing.T) {
-	optionsData := []byte{
-		0x01,                   // NOP
-		0x07, 0x07, 0x12, 0x34, // Record Route option with example data
-		0x00, // End of list
+	ipPacket := []byte{
+		0x49, 0x00, 0x00, 0x28, 0x03, 0x04,
+		0x00, 0x00, 0xfe, 0x01, 0xc1, 0xe0,
+		0xaf, 0x2d, 0xb0, 0x00, 0x95, 0xab,
+		0x7e, 0x0b, 0x01, 0x82, 0x0b, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x01, 0x02, 0x03,
 	}
 
-	options, err := ParseOptions(optionsData)
+	header, err := ParseIPv4Header(ipPacket)
 	if err != nil {
-		t.Error("cannot parse options", err)
+		t.Error("Cannot parse header", err)
+		return
 	}
 
-	if len(options) != 1 {
-		t.Error("Invalid options")
+	if header.ProtocolString() != "ICMP" {
+		t.Errorf("Invalid protocol string: %s", header.ProtocolString())
+		return
 	}
 
-	if options[0].Type != uint8(OptionRecordRoute) {
-		t.Error("Invalid option type")
+	options, err := header.ParseOptions()
+	if err != nil {
+		t.Error("Cannot parse options", err)
+		return
 	}
 
-	if options[0].TypeShort() != "RR" {
-		t.Error("Invalid option type")
+	if len(options) != 2 {
+		t.Error("Invalid options len", options)
+	}
+
+	firstOption := options[0]
+
+	if firstOption.GetType() != OptionNoOperation {
+		t.Error("Invalid first option. Should NoOp", firstOption.GetType())
+		return
+	}
+
+	if firstOption.Length != 1 {
+		t.Error("Invalid first option len. Should 1", firstOption.Length)
+		return
+	}
+
+	if len(firstOption.Data) != 0 {
+		t.Error("Invalid first option NoOp should not have data", len(firstOption.Data))
+		return
+	}
+
+	if firstOption.TypeShort() != "NOP" {
+		t.Error("Invalid first option. Should NOP", firstOption.TypeShort())
+	}
+
+	secondOption := options[1]
+
+	if secondOption.GetType() != OptionSecurityRIPSO {
+		t.Error("Invalid second option. Should Security RIPSO", secondOption.GetType())
+		return
+	}
+
+	if len(secondOption.Data) != 9 {
+		t.Error("Invalid second option. Should have len 9", len(firstOption.Data))
+		return
+	}
+
+	if secondOption.TypeShort() != "SEC" {
+		t.Error("Invalid second option. Should SEC", secondOption.TypeShort())
 	}
 }
